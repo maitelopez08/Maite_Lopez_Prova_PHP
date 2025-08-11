fbeeefdddccece21942d1cf58f4472e8c3b00784
Maite <maite_azanza@estudante.sesisenai.org.br>
2025-08-11 17:14:11 -0300

    busca cliente

--- buscar_usuario.php
+++ buscar_usuario.php
@@ -1,6 +1,69 @@
 <?php
+session_start();
+require_once 'conexao.php';
 
+//VERIFICA SE O USUARIO TEM PRMISSAO DE adm OU Secretaria
+if($_SESSION['perfil'] !=1 && $_SESSION['perfil']!=2){
+    echo "<script>alert('Ácesso negado!');window.location.href='principal.php'</script>";
+    exit();
+}
 
+$usuario = []; //INICIALIZA A VARIAVEL PARA EVITAR ERROS
 
+//SE O FORMULARIO FOI ENVIADO, BUSCA O USUARIO PELO ID OU NOME
+if($_SERVER["REQUEST_METHOD"]=="POST" && !empty($_POST['busca'])){
+    $busca = trim($_POST['busca']);
 
+    //VERIFICA SE A BUSCA É UM numero OU UM nome
+    if(is_numeric($busca)){
+        $sql="SELECT * FROM usuario WHERE id_usuario = :busca ORDER BY nome ASC";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':busca',$busca, PDO::PARAM_INT);
+    }else{
+        $sql="SELECT * FROM usuario WHERE nome LIKE :busca_nome ORDER BY nome ASC";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':busca_nome',"%$busca%", PDO::PARAM_STR);
+    }
+}else{
+        $sql="SELECT * FROM usuario ORDER BY nome ASC";
+        $stmt = $pdo->prepare($sql);
+}
+$stmt->execute();
+$usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Usuário</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2>Lista de Usuários</h2>    
+    <form action="buscar_usuario.php" method="POST">
+        <label for="busca">Digite o ID ou NOME(opcional):</label>
+        <input type="text" id="busca" name="busca">
+    </form>
+        <?php if(!empty($usuarios)):?>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Perfil</th>
+                    <th>Ações</th>
+                </tr>
+                <?php foreach($usuarios as $usuario): ?>
+      
+                <tr>
+                    <td><?=htmlspecialchars($usuario['id_usuario'])?></td>
+                    <td><?=htmlspecialchars($usuario['nome'])?></td>
+                    <td><?=htmlspecialchars($usuario['email'])?></td>
+                    <td><?=htmlspecialchars($usuario['id_perfil'])?></td>
+                </tr>
+
+      </table>
+
+</body>
+</html>
--- principal.php
+++ principal.php
@@ -80,7 +80,7 @@ $opcoes_menu = $permissoes["$id_perfil"];
                 <ul class="dropdown-menu">
                     <?php foreach($arquivos as $arquivo):?>
                         <li>
-                        <a href="<?=$arquivo?>"><?=ucfirst(str_replace("_","",basename($arquivo, ".php")))?></a>
+                        <a href="<?=$arquivo?>"><?=ucfirst(str_replace("_"," ",basename($arquivo, ".php")))?></a>
                         </li>
                     <?php endforeach;?>
                 </ul>
