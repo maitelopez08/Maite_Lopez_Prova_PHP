af136bfe3ed53b25a7b7d2324080e7ab8592da9e
Maite <maite_azanza@estudante.sesisenai.org.br>
2025-08-21 16:47:06 -0300

    Update validacao_alterar.js
    Validação alterar usuário

--- validacao_alterar.js
+++ validacao_alterar.js
@@ -1,8 +1,6 @@
 function validarAlteracao() {
     let nome = document.getElementById("nome").value.trim();
     let email = document.getElementById("email").value.trim();
-    let perfil = document.getElementById("id_perfil").value;
-    let novaSenha = document.getElementById("nova_senha") ? document.getElementById("nova_senha").value : "";
 
     if (nome.length === 0) {
         alert("O campo Nome é obrigatório.");
